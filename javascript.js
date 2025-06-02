e69c342d2c67a4a520767974aad533d2f1329cb4
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-06-02 17:38:17 +0200

    Create operate function that calculates with input
    Create operate function that takes two numbers and an operater and
    passes them to one of the calculation functions and returns the result.
    In the calculation functions turn the first and second number with
    Number to a number to make calculation work

--- javascript.js
+++ javascript.js
@@ -1,15 +1,38 @@
+const firstNumber = "0123456789";
+const secondNumber = "0123456789";
+const operator = "+-*/";
+
+function operate(firstNumber, operator, secondNumber) {
+    switch(operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+            break;
+        case "-":
+            return subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            return multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            return divide(firstNumber, secondNumber);
+            break;
+        default:
+            alert("Wrong value, please use +, -, * or /")
+    }
+}
+
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return Number(a) - Number(b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return Number(a) * Number(b);
 }
 
 function divide(a, b) {
-    return a / b;
+    return Number(a) / Number(b);
 }
